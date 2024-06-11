850461b36d9490920f310d587999ecac7005b304
rulirhs <137042002+rulirhs@users.noreply.github.com>
2024-06-11 23:07:35 +0700

    18_Progke-2_ProjectUAS.py

--- Genetic Algorithm/GA.py
+++ Genetic Algorithm/GA.py
@@ -20,7 +20,7 @@ data = data.dropna()
 X = data.drop(['id', 'satisfaction'], axis=1)
 y = data['satisfaction']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
 
 nb_model = GaussianNB()
 nb_model.fit(X_train, y_train)
